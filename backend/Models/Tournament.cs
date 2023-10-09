c7c2ace5ade037a3511f66a249541353af67ca17
Stingeder <72016024+Stingeder@users.noreply.github.com>
2023-10-09 19:15:33 +0200

    Added Tournament and Users Models

---
+++ backend/Models/Tournament.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+
+    public record CreateTournamentDTO(string Name, int Type);
+}
---
+++ backend/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Type { get; set; }
+        public int TournamentId { get; set; }
+        public string ConnectionString { get; set; }
+    }
+
+    public record CreateUserDTO(string Username, string Password, int Type, int TournamentId, string CurrentUser);
+}
+
