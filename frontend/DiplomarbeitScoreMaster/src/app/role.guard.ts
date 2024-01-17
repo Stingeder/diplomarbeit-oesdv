633b02ca089c554ee1e9594087ed421f65f4b7c2
Stingeder <72016024+Stingeder@users.noreply.github.com>
2024-01-17 16:50:08 +0100

    Added role guard

---
+++ frontend/DiplomarbeitScoreMaster/src/app/role.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  constructor(private router: Router, private keycloakService: KeycloakService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    const requiredRoles = route.data['requiredRoles'] as string[];
+
+    // Check if the user is authenticated
+    if (this.keycloakService.isLoggedIn()) {
+      const userRoles = this.keycloakService.getUserRoles();
+
+      // Check if the user has any of the required roles
+      const hasRequiredRole = requiredRoles.some((role) => userRoles.includes(role));
+
+      if (hasRequiredRole) {
+        // User has the required role, allow access
+        return true;
+      } else {
+        // User does not have the required role, redirect them to a forbidden page or another route
+        return this.router.createUrlTree(['/forbidden']);
+      }
+    } else {
+      // User is not authenticated, redirect to the login page
+      return this.router.createUrlTree(['/login']);
+    }
+  }
+}
